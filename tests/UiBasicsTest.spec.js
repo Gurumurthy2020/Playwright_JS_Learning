3873925f23b0a4e5a79c6753cf4773f545d3add9
Gurumurthy2020 <guru051217@gmail.com>
2025-07-20 16:41:48 -0500

    Initial commit to ishu

--- tests/UiBasicsTest.spec.js
+++ tests/UiBasicsTest.spec.js
@@ -6,32 +6,12 @@ test('Browser context Playwright test case',async ({browser})=>
 {
     const context=await browser.newContext();
     const page= await context.newPage();
-    
-    const username=page.locator("[name='username']");
-    const submitbutton=page.locator("[type='submit']");
-    const alltext=page.locator(".card-body a");
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");     
-    console.log(await page.title());
-    await username.fill("rahulshetty");
-    await page.locator("[name='password']").fill("learning");
-    await page.locator("[type='checkbox']").click();
-    await submitbutton.click();
-    console.log(await page.locator("[style*='block']").textContent());
-    await expect(page.locator("[style*='block']")).toContainText("Incorrect username/password.");
-    await username.fill("rahulshettyacademy");
-    await submitbutton.click();
-
-    //console.log(await alltext.nth(0).textContent());
-    console.log(await alltext.allTextContents());
-    
-
-
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");               
 })
 
 test('Page  Playwright test case',async ({page})=>
 {
    
-<<<<<<< HEAD
     await page.goto("https://google.com");               
 })
 test('@Web Client App login', async ({ page }) => {
@@ -104,58 +84,3 @@ test('@Child windows hadl', async ({browser})=>
   await page.getByRole('button', { name: 'Sign In' }).click();
 });
  
-=======
-    await page.goto("https://google.com");   
-    console.log(await page.title());
-    await expect (page).toHaveTitle("Google");
-})
-
-test('UI controls',async({page})=>{
-    const username=page.locator("[name='username']");
-    const submitbutton=page.locator("[type='submit']");
-
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    await username.fill("rahulshettyacademy");
-    await page.locator("[name='password']").fill("learning");
-    await page.locator("select.form-control").selectOption("consult");
-    await page.locator("span.checkmark").last().click();
-    await page.locator("#okayBtn").click();
-    await expect(page.locator("span.checkmark").last()).toBeChecked();
-    await page.locator("[name=terms]").click();
-    await expect(page.locator("[name=terms]")).toBeChecked();
-    await page.locator("[name=terms]").uncheck();
-    await page.pause();
-    expect(await page.locator("[name=terms]").isChecked()).toBeFalsy();
-    await expect(page.locator("[target='_blank']")).toHaveAttribute('class','blinkingText');
-
-    await submitbutton.click();
-
-})
-
-test.only('Child window handling ',async({browser})=>{
-    const context       = await browser.newContext();
-    const page          = await context.newPage();
-     const username=page.locator("[name='username']");
-
-    await page.goto("https://rahulshettyacademy.com/loginpagePractise/");
-    const documentLink  = page.locator("[href*='documents-request']");
- 
-// func for direct one tab to the link
- 
-    const [newPage] =await Promise.all([
-    context.waitForEvent('page'),
-    documentLink.click(),
- 
-])
-    const text= await newPage.locator(".red").textContent();
-    console.log(text);
-    const arraytext=text.split("@");
-    const domain=arraytext[1].split(" ")[0];
-    console.log(await domain.trim());
-     await page.locator("#username").fill(domain);
-    const value = await page.locator("#username").inputValue();
-    console.log(value);
-
-    });
-
->>>>>>> 1cd10d71a3734644a3c25f72799b8c48f16b1286
